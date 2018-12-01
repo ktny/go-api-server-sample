56a11e879d66e1c8884c8957c89ebb51ce467913
Katsunori Takahashi <katsunori.t10@gmail.com>
2018-12-02 00:25:13 +0900

    add: シンプルなAPIサーバー

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+// curl http://localhost:8888/hello/world
+func getHello(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	lang := p.ByName("lang")
+	fmt.Fprintf(w, "hello %s\n", lang)
+}
+
+// curl -X POST -d "{\"id\": 1, \"name\": \"ktny\"}" http://localhost:8888/hello
+func postHello(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	defer r.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	type SampleParameter struct {
+		ID   int
+		Name string
+	}
+	var param SampleParameter
+
+	err = json.Unmarshal(bodyBytes, &param)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(w, fmt.Sprintf("%+v\n", param))
+}
+
+func main() {
+	router := httprouter.New()
+
+	router.GET("/hello/:lang", getHello)
+	router.POST("/hello", postHello)
+
+	err := http.ListenAndServe(":8888", router)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
